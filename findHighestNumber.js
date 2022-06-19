1e56640e581b1531f116e395451df0bdfb290c2e
Monsoud Zanaty III <monsoud@Monsouds-Air.fios-router.home>
2022-06-19 11:06:53 -0400

    new problems added

--- findHighestNumber.js
+++ findHighestNumber.js
@@ -1,7 +1,10 @@
 const array = [200,400,53,62,563,2,1,600]
 
+//MAX
 let max = Math.max.apply(0, array)
 console.log(max)
+
+//drawing it out
 
 let largest = array[0];
     for (let i = 0; i < array.length; i++){
--- secondHighestNumber.js
+++ secondHighestNumber.js
@@ -1,14 +1,14 @@
 const array = [200,400,53,62,563,2,1,600]
 
-
+let highest = array[0];
 let secondHighest = array[0];
-for (let i = 0; i < array.length; i++){
-
-    if(secondHighest < array[i]) {
-        secondHighest = array[i]
+for(let i = 0; i < array.length; i++){
+    if(array[i] > highest){
+        secondHighest = highest;
+        highest = array[i];
     }
+    else if(array[i] > secondHighest && array[i] != highest)
+        secondHighest = array[i];
 }
 
-console.log(secondHighest)
-
-// need to work on this^ 06/14/2022
+console.log(secondHighest);
